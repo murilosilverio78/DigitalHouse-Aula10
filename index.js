68faa1fe52415ca065fedf3d657007681f895b7b
murilosilverio78 <murilo.silverio@hotmail.com>
2021-03-03 20:42:10 -0300

    adicionando hello world

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.get('/', (req, res) => {
+    return res.send("Hello World")
+})
+
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
